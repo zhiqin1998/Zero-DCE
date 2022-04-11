555dc0388cdf8615dee956ab49237772240dc4dd
Tan Zhi Qin <zhiqin1998@hotmail.com>
2022-04-11 22:07:54 +0800

    added n16 variant model

--- lowlight_test.py
+++ lowlight_test.py
@@ -21,7 +21,7 @@ def lowlight(image_path):
     data_lowlight = data_lowlight.permute(2, 0, 1)
     data_lowlight = data_lowlight.cuda().unsqueeze(0)
 
-    DCE_net = model.enhance_net_nopool().cuda()
+    DCE_net = model.enhance_net_nopool_n16().cuda()
     DCE_net.load_state_dict(torch.load('snapshots/Epoch99.pth'))
     start = time.time()
     _, enhanced_image, _ = DCE_net(data_lowlight)
