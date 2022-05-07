d322259eecb164236f0b26a9ab5047d5eeec7660
Tan Zhi Qin <zhiqin1998@hotmail.com>
2022-05-07 15:48:18 +0800

    fixed con model bug

--- model.py
+++ model.py
@@ -89,7 +89,7 @@ class enhance_net_nopool_con(nn.Module):
         x_r = F.tanh(self.e_conv7(torch.cat([x1, x6], 1)))
         r1, r2, r3, r4, r5, r6, r7, r8 = torch.split(x_r, 3, dim=1)
 
-        x = x + r1 * (torch.pow(x, 2) - x)
+        x = low_con + r1 * (torch.pow(low_con, 2) - low_con)
         x = x + r2 * (torch.pow(x, 2) - x)
         x = x + r3 * (torch.pow(x, 2) - x)
         enhance_image_1 = x + r4 * (torch.pow(x, 2) - x)
